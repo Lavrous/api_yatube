e9ab2485b87895e87410ed01a51e84f1a1e33655
Евгений Коваленко <zenakovalenko1@gmail.com>
2026-03-19 10:39:59 +0700

    Yandex stuff

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,8 +8,11 @@ from api.views import PostViewSet, GroupViewSet, CommentViewSet
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
 router.register('groups', GroupViewSet, basename='groups')
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
-
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token, name='api_token_auth'),
     path('v1/', include(router.urls)),
